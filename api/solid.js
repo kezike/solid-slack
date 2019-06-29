2f080548f08c1d893d25c4e177851787d165ef71
kezike <kezike13@gmail.com>
2019-06-29 02:48:18 -0400

    Testing out weather report

--- api/solid.js
+++ api/solid.js
@@ -1,7 +1,15 @@
-const moment = require('moment')
+// const moment = require('moment')
 
 module.exports = (req, res) => {
-  const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
-
-  res.end(currentTime)
+  // const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+  const weather = {
+      "response_type": "in_channel",
+      "text": "It's 80 degrees right now.",
+      "attachments": [
+        {
+          "text":"Partly cloudy today and tomorrow"
+        }
+      ]
+  };
+  res.json(weather);
 }
