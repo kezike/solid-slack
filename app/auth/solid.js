f4d56f11d69c77198771bd78a1a6e435e3e4d585
kezike <kezike13@gmail.com>
2021-03-13 20:38:50 -0500

    using response_url to notify user of unauthenticated state

--- app/auth/solid.js
+++ app/auth/solid.js
@@ -6,14 +6,20 @@ const { slackIdToSolidClient } = require('./common');
 const { slackClient } = require('./slack');
 
 const solidVerify = async (req, res, next) => {
-  const slackUserId = req.slack.user_id;
-  console.log("SLACK USER ID:", slackUserId);
-  let solidClient = slackIdToSolidClient[slackUserId];
+  const userId = req.slack.user_id;
+  const responseUrl = req.slack.response_url;
+  console.log("SLACK USER ID:", userId);
+  console.log("RESPONSE URL:", responseUrl);
+  let solidClient = slackIdToSolidClient[userId];
   if (!solidClient || !solidClient.session.loggedIn) {
     console.log('Unauthenticated User');
     try {
       // const loginCommandStatus = await LoginManager.exec(slackClient/*, commands*/, req.slack/*, res*/);
-      return res.status(httpStatus.UNAUTHORIZED).send("You are not yet connected to a valid Solid account! Please run the following command to login: `/solid login`");
+      const chatPayload = {
+        text: "Please run the following command to login to Solid: `/solid login`",
+      };
+      await slackClient.axios.post(responseUrl, chatPayload);
+      return res.status(httpStatus.UNAUTHORIZED).send("User is not authenticated to a Solid account");
     } catch (e) {
       console.error(JSON.stringify(e, null, 4));
       return res.status(httpStatus.BAD_REQUEST).json(e);
