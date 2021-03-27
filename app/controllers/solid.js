65d9fe421cd5ca376c3d2770b213d539e87649b2
kezike <kezike13@gmail.com>
2021-03-27 09:32:52 -0400

    provided message in res.send() during login form submission

--- app/controllers/solid.js
+++ app/controllers/solid.js
@@ -67,7 +67,7 @@ const solidLogin = async (req, res) => {
       };
       await slackClient.axios.post(responseUrl, chatPayload);
       // return res.status(httpStatus.OK).send();
-      return res.send();
+      return res.send('Congratulations: you have successfully logged into Solid!');
     }
     return res.status(httpStatus.OK).send('We were not able to authenticate you to your Solid account. Please double check your credentials and try again.');
   } catch (e) {
