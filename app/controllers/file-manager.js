f9c90c84aa79e1794b1115adce11f8931a13325d
kezike <kezike13@gmail.com>
2021-03-26 22:10:19 -0400

    switched from clone to cloneDeep

--- app/controllers/file-manager.js
+++ app/controllers/file-manager.js
@@ -52,7 +52,7 @@ class FileManager {
 
   static async loadProfile(req, res) {
     try {
-      fileViewer = _.clone(fileViewer);
+      fileViewer = _.cloneDeep(fileViewer);
       const trigger_id = req.body.trigger_id;
       const token = slackClient.token;
       const userId = req.body.user_id;
