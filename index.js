d19a3c1b485998e0524dcba79527aa570b92427b
kezike <kezike13@gmail.com>
2021-04-05 00:34:32 -0400

    officially reverting all attempts to configure now/vercel deployments, as it is not a good fit for this app

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const { httpStatus } = require('./app/util/http');
 const { commandVerify } = require('./app/middlewares/commands');
 const { slackClient, slackVerify } = require('./app/middlewares/slack');
 const { solidVerify } = require('./app/middlewares/solid');
-const entryHandler = require('./api/entry');
-const actionHandler = require('./api/action');
 
 // Main app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/*// Handle app entrypoint
+// Handle app entrypoint
 const entryHandler = async (req, res) => {    
   const commands = req.commands;
   const command = commands[0];
@@ -49,7 +47,7 @@ const actionHandler = async (req, res) => {
     default:
       return res.status(httpStatus.OK).send(`Unrecognized interactive component \`callback_id\`: \`${callbackId}\``);
   }
-};*/
+};
 
 // Parse body as json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -75,8 +73,3 @@ app.use(solidVerify);
 app.post('/entry', entryHandler);
 
 app.listen(PORT, () => console.log(`Solid Slack listening at http://0.0.0.0:${PORT}`));
-
-module.exports = {
-  entryHandler,
-  actionHandler,
-};
