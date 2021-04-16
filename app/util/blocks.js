d468f0f7b92e146a4ab030932f58d27dcc946587
kezike <kezike13@gmail.com>
2021-04-16 07:32:10 -0400

    test further reduced profile card in plain_text_input block

--- app/util/blocks.js
+++ app/util/blocks.js
@@ -344,10 +344,10 @@ const addContainerBlocks = (viewConfig, statements) => {
     @prefix pro: <./>.
     @prefix n0: <http://xmlns.com/foaf/0.1/>.
     @prefix schem: <http://schema.org/>.
-    @prefix n1: <http://www.w3.org/ns/auth/acl#>.
+    @prefix n1: <http://www.w3.org/ns/auth/acl#>.`,/*
     @prefix ldp: <http://www.w3.org/ns/ldp#>.
     @prefix inbox: </inbox/>.
-    @prefix sp: <http://www.w3.org/ns/pim/space#>.`,/*
+    @prefix sp: <http://www.w3.org/ns/pim/space#>.
     @prefix kez: </>.
     @prefix r: <https://example.com/random#>.
     @prefix sec: <https://w3id.org/security#>.
