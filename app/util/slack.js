ef9a3c08e85c1fad6878e77cfc0de3ea0d63dc77
kezike <kezike13@gmail.com>
2021-04-20 09:35:39 -0400

    properly export FILE_SIZE_LIMIT

--- app/util/slack.js
+++ app/util/slack.js
@@ -4,4 +4,4 @@ const slackClient = new WebClient(slackToken);
 const FILE_SIZE_LIMIT = 3000;
 const VIEW_STACK_LIMIT = 3;
 
-module.exports = { slackClient, VIEW_STACK_LIMIT };
+module.exports = { slackClient, FILE_SIZE_LIMIT, VIEW_STACK_LIMIT };
